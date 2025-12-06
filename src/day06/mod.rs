8776f0011bbcf8c4a8cecd009b3acf110737d014
Nignik <maksymilian.olbrys@gmail.com>
2025-12-06 15:16:32 +0100

    day 1, star 1

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -1,3 +1,44 @@
-pub fn solve() {
-  
+pub fn star1(raw_data: String) -> String {
+  let data = parse_input(&raw_data);
+
+  let mut res = 0;
+  for (op, values) in data {
+    res += match op {
+      '*' => values.iter().product(),
+      '+' => values.iter().sum(),
+      _ => 0,
+    };
+  }
+
+  format!("{}", res)
+}
+
+pub fn star2(raw_data: String) -> String {
+  let mut res = 0;
+
+  format!("{}", res)
+}
+
+fn parse_input(raw_data: &str) -> Vec<(char, Vec<u64>)> {
+  let mut data: Vec<(char, Vec<u64>)> = Vec::new();
+  for line in raw_data.lines() {
+    let s: Vec<&str> = line.split_whitespace().collect();
+
+    if data.is_empty() {
+      for _ in 0..s.len() {
+        data.push((' ', Vec::new()));
+      }
+    }
+
+    for (i, x) in s.iter().enumerate() {
+      let (op, nums) = &mut data[i];
+      match x.chars().nth(0) {
+        Some(y) if y.is_numeric() => nums.push(x.parse().unwrap()),
+        Some(y) => *op = y,
+        _ => continue,
+      }
+    }
+  }
+
+  data
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ use seq_macro::seq;
 use std::env;
 use std::fs;
 
-seq!(N in 01..=12 {
+seq!(N in 06..=06 {
   mod day~N;
 });
 
-pub fn read_input(day: u8) -> String {
-  let path = format!("input/day{:02}.txt", day);
-  fs::read_to_string(&path).unwrap_or_else(|_| panic!("Could not read input file: {}", path))
+pub fn read_input(day: u8, file_name: &str) -> String {
+  let path = format!("input/day{:02}/{}", day, file_name);
+  let absolute_path = std::path::absolute(&path).unwrap();
+  fs::read_to_string(&path)
+    .unwrap_or_else(|_| panic!("Could not read input file: {}", absolute_path.display()))
 }
 
 fn main() {
@@ -17,10 +19,17 @@ fn main() {
     .get(1)
     .expect("Please provide a day number (e.g., '01')");
 
-  seq!(N in 01..=12 {
+  seq!(N in 06..=06 {
     match day_arg.as_str() {
       #(
-        stringify!(N) => day~N::solve(),
+        stringify!(N) => {
+          println!("===== Example =====");
+          println!("star 1: {}", day~N::star1(read_input(N, "example.in")));
+          println!("star 2: {}", day~N::star2(read_input(N, "example.in")));
+          println!("===== Input =====");
+          println!("star 1: {}", day~N::star1(read_input(N, "input.in")));
+          println!("star 2: {}", day~N::star2(read_input(N, "input.in")));
+        }
       )*
       _ => println!("Day {} not solved yet!", day_arg),
     }
