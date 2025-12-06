ab9f9e572fed8e9fd1ebc552f101144a79abf95b
Nignik <maksymilian.olbrys@gmail.com>
2025-12-06 17:40:31 +0100

    day 6

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -15,6 +15,87 @@ pub fn star1(raw_data: String) -> String {
 
 pub fn star2(raw_data: String) -> String {
   let mut res = 0;
+  let mut ops: Vec<char> = Vec::new();
+  let mut nums: Vec<String> = Vec::new();
+  let lines: Vec<&str> = raw_data.lines().rev().collect();
+  let max_width = lines.iter().map(|s| s.len()).max().unwrap_or(0);
+
+  for (i, line_iter) in lines.iter().enumerate() {
+    let mut line: Vec<char> = line_iter.chars().collect();
+    line.extend(vec![' '; max_width - line.len()]);
+
+    if i == 0 {
+      nums = vec![String::new(); line.len()];
+      ops.extend(line);
+      continue;
+    }
+
+    for (c, num) in std::iter::zip(line, &mut nums) {
+      if c.is_alphanumeric() {
+        num.push(c);
+      }
+    }
+  }
+
+  let mut prev_op = 'x';
+  let mut partial = 0;
+  for (op, num_str) in std::iter::zip(ops, nums) {
+    let num: u128 = num_str
+      .chars()
+      .rev()
+      .collect::<String>()
+      .parse()
+      .unwrap_or(0);
+    if num == 0 {
+      prev_op = 'x';
+      continue;
+    }
+
+    if op == prev_op || op == ' ' {
+      match prev_op {
+        '*' => partial *= num,
+        '+' => partial += num,
+        _ => {}
+      };
+    } else {
+      res += partial;
+      prev_op = op;
+      partial = num;
+    }
+  }
+  res += partial;
+
+  format!("{}", res)
+}
+
+pub fn star(raw_data: String) -> String {
+  let lines: Vec<Vec<char>> = raw_data
+    .lines()
+    .rev()
+    .map(|line| line.chars().collect())
+    .collect();
+
+  let width = lines.iter().map(|s| s.len()).max().unwrap_or(0);
+  let height = lines.len()-1;
+  let mut right = width;
+  let res = (0..width).rev().filter(|&col| lines[height][col] != ' ').fold(
+    0, |acc, left| {
+      let cols = (left..right)
+        .map(|row| (0..height)
+        .fold(0, |num, col| {
+          match lines[row][col].to_digit(10) {
+            Some(digit) => 10*num + digit as u128,
+            None => num
+          }
+        }));
+      right = left.saturating_sub(1);
+      acc + match lines[height][left] {
+        '+' => cols.sum(),
+        '*' => cols.product(),
+        _ => 0
+      }
+    }
+  );
 
   format!("{}", res)
 }
