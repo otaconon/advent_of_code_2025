d1e5b91f63ebe6a5e8a35477b329f52752622d74
Nignik <maksymilian.olbrys@gmail.com>
2025-12-09 14:59:41 +0100

    no floats day 9

--- src/day09/mod.rs
+++ src/day09/mod.rs
@@ -1,5 +1,5 @@
 fn field(a: &Point, b: &Point) -> i64 {
-  ((f64::abs(a.x - b.x) + 1.0) * (f64::abs(a.y - b.y) + 1.0)) as i64
+  (i64::abs(a.x - b.x) + 1) * (i64::abs(a.y - b.y) + 1)
 }
 
 pub fn star1(raw_data: String) -> String {
@@ -16,28 +16,28 @@ pub fn star1(raw_data: String) -> String {
 
 #[derive(Debug, Clone, Copy)]
 struct Point {
-  x: f64,
-  y: f64,
+  x: i64,
+  y: i64,
 }
 
 #[derive(Debug, Clone)]
 enum AxisAlignedLine {
-  Vertical { x: f64, y_min: f64, y_max: f64 },
-  Horizontal { y: f64, x_min: f64, x_max: f64 },
+  Vertical { x: i64, y_min: i64, y_max: i64 },
+  Horizontal { y: i64, x_min: i64, x_max: i64 },
 }
 
 fn segment_cuts_rect(
-  rect_x_min: f64,
-  rect_x_max: f64,
-  rect_y_min: f64,
-  rect_y_max: f64,
+  rect_x_min: i64,
+  rect_x_max: i64,
+  rect_y_min: i64,
+  rect_y_max: i64,
   segment: &AxisAlignedLine,
 ) -> bool {
   match segment {
     AxisAlignedLine::Vertical { x, y_min, y_max } => {
-      if *x > rect_x_min + f64::EPSILON && *x < rect_x_max - f64::EPSILON {
-        let intersect_y_min = f64::max(*y_min, rect_y_min);
-        let intersect_y_max = f64::min(*y_max, rect_y_max);
+      if *x > rect_x_min && *x < rect_x_max {
+        let intersect_y_min = i64::max(*y_min, rect_y_min);
+        let intersect_y_max = i64::min(*y_max, rect_y_max);
         if intersect_y_max > intersect_y_min {
           return true;
         }
@@ -45,9 +45,9 @@ fn segment_cuts_rect(
       false
     }
     AxisAlignedLine::Horizontal { y, x_min, x_max } => {
-      if *y > rect_y_min + f64::EPSILON && *y < rect_y_max - f64::EPSILON {
-        let intersect_x_min = f64::max(*x_min, rect_x_min);
-        let intersect_x_max = f64::min(*x_max, rect_x_max);
+      if *y > rect_y_min && *y < rect_y_max {
+        let intersect_x_min = i64::max(*x_min, rect_x_min);
+        let intersect_x_max = i64::min(*x_max, rect_x_max);
         if intersect_x_max > intersect_x_min {
           return true;
         }
@@ -68,15 +68,15 @@ pub fn star2(raw_data: String) -> String {
     if a.x == b.x {
       segments.push(AxisAlignedLine::Vertical {
         x: a.x,
-        y_min: f64::min(a.y, b.y),
-        y_max: f64::max(a.y, b.y),
+        y_min: i64::min(a.y, b.y),
+        y_max: i64::max(a.y, b.y),
       })
     }
     if a.y == b.y {
       segments.push(AxisAlignedLine::Horizontal {
         y: a.y,
-        x_min: f64::min(a.x, b.x),
-        x_max: f64::max(a.x, b.x),
+        x_min: i64::min(a.x, b.x),
+        x_max: i64::max(a.x, b.x),
       })
     }
   }
@@ -85,10 +85,10 @@ pub fn star2(raw_data: String) -> String {
     for b in &points {
       let mut intersects = false;
       for segment in &segments {
-        let x_min = f64::min(a.x, b.x);
-        let x_max = f64::max(a.x, b.x);
-        let y_min = f64::min(a.y, b.y);
-        let y_max = f64::max(a.y, b.y);
+        let x_min = i64::min(a.x, b.x);
+        let x_max = i64::max(a.x, b.x);
+        let y_min = i64::min(a.y, b.y);
+        let y_max = i64::max(a.y, b.y);
         intersects |= segment_cuts_rect(x_min, x_max, y_min, y_max, segment);
       }
       if !intersects {
