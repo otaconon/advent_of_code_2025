83b79413fc871aa63018b2793dd2766a72b8a7a4
Nignik <maksymilian.olbrys@gmail.com>
2025-12-11 19:30:40 +0100

    day 11

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -1,30 +1,60 @@
-use std::{collections::{HashMap, HashSet}, hash::Hash, u32};
+use std::{
+  collections::{HashMap, HashSet, VecDeque},
+  hash::Hash,
+  u64,
+};
 
-pub fn star1(raw_data: String) -> String {
-  let adj = parse_input(&raw_data);
-  let mut ans = 0;
+fn count_paths<'a>(start: &'a str, end: &'a str, adj: &HashMap<&'a str, Vec<&'a str>>) -> u64 {
+  let mut memo: HashMap<&'a str, u64> = HashMap::new();
+  dfs(start, end, adj, &mut memo)
+}
 
-  let mut st: Vec<&str> = Vec::new();
-  let vis: HashSet<&str> = HashSet::new();
-  let mut cnt: HashMap<&str, u32> = HashMap::new();
-  st.push("you");
-  while !st.is_empty() {
-    let u = st.pop().unwrap();
-    for v in &adj[u] {
-      if !vis.contains(v) {
-        st.push(v);
-      }
-      *cnt.entry(v).or_insert(0) += 1;
+fn dfs<'a>(
+  curr: &'a str,
+  target: &'a str,
+  adj: &HashMap<&'a str, Vec<&'a str>>,
+  memo: &mut HashMap<&'a str, u64>,
+) -> u64 {
+  if curr == target {
+    return 1;
+  }
+
+  if let Some(&count) = memo.get(curr) {
+    return count;
+  }
+
+  let mut total_paths = 0;
+  if let Some(neighbors) = adj.get(curr) {
+    for &next_node in neighbors {
+      total_paths += dfs(next_node, target, adj, memo);
     }
   }
 
-  format!("{}", cnt["out"])
+  memo.insert(curr, total_paths);
+  total_paths
+}
+
+pub fn star1(raw_data: String) -> String {
+  let adj = parse_input(&raw_data);
+  format!("{}", count_paths("you", "out", &adj))
+}
+
+fn calculate_path<'a>(path: Vec<&'a str>, adj: &HashMap<&'a str, Vec<&'a str>>) -> u64 {
+  let mut res = 1;
+  for node in 0..path.len() - 1 {
+    res *= count_paths(path[node], path[node + 1], &adj);
+  }
+  res
 }
 
 pub fn star2(raw_data: String) -> String {
+  let adj = parse_input(&raw_data);
+  let mut ans = 0;
 
+  ans += calculate_path(vec!["svr", "fft", "dac", "out"], &adj);
+  ans += calculate_path(vec!["svr", "dac", "fft", "out"], &adj);
 
-  format!("{}", 0)
+  format!("{}", ans)
 }
 
 fn parse_input(raw_data: &str) -> HashMap<&str, Vec<&str>> {
@@ -38,5 +68,3 @@ fn parse_input(raw_data: &str) -> HashMap<&str, Vec<&str>> {
 
   data
 }
-
-
